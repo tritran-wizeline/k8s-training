93bc3640f41ed1feca0ea9e9572620f0afef362b
Tri Tran <tri.tran@wizeline.com>
2020-07-06 23:08:07 +0700

    Session 3

---
+++ demos/session3/session3.sh
@@ -0,0 +1,54 @@
+### Labels, selectors, annotations
+# creating pod with label
+k apply -f my-label-pod.yml
+k describe pod my-label-pod
+k get pods -l app=my-app
+
+# creating pod with annotations
+k apply -f my-annotation-pod.yml
+k describe pod my-annotation-pod
+
+# creating a deployment
+k apply -f nginx-deployment.yml
+k describe deployment nginx-deployment
+k get pods -l app=nginx
+# updating deployment, like increasing number of replicas
+k edit deployment nginx-deployment
+k get pods -l app=nginx
+
+### Rolling updates, rollbacks
+# doing rolling update
+k set image deployment nginx-deployment nginx=nginx:1.18.0 --record
+# get rollout history of deployment
+k rollout history deployment nginx-deployment
+# roll back deployment to previous version
+k rollout undo deployment nginx-deployment
+k describe deployment nginx-deployment
+
+### Services
+# exposing deployment as a ClusterIP service
+k apply -f my-clusterip-service.yml
+# accessing ClusterIP service using an internal pod in k8s cluster
+k run test --image=busybox --restart=Never --rm -it -- sh
+wget -O- [Cluster_IP]:8080
+
+# alternative way of exposing deployment as a service
+k expose deployment nginx-deployment --type=ClusterIP --port=8080 --target-port=80
+
+# exposing deployment as a NodePort service
+k apply -f my-nodeport-service.yml
+# accessing NodePort service
+curl $(minikube_ip):[node_port]
+
+### State persistence
+# Creating emptyDir volumes
+k apply -f volume-pod.yml
+# Creating PersistentVolume
+k apply -f my-pv.yml
+k get pv
+# Creating PersistentVolumeClaim
+k apply -f my-pvc.yml
+k get pvc
+# Creating a pod using PersistentVolumeClaim
+k apply -f my-pvc-pod.yml
+k get pods
